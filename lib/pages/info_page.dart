0a3e84ee19e0f40f2f828626a62e296f37b7bed8
cakra <cakra.ikhwan09@gmail.com>
2026-03-12 05:59:27 +0700

    Update info_page.dart

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -13,12 +13,12 @@ class InfoPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Text(
-              'Ini StatelessWidget',
+              'Praktikum Pemrograman Mobile',
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 8),
             Text(
-              'Halaman ini tidak menyimpan state yang berubah.',
+              '2406177 - Cakra Nur Ikhwan',
               style: TextStyle(fontSize: 16),
             ),
           ],
